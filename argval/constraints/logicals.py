d8af84bfb8ab1f0d2611ae06e7e1968827e80810
Chenchao Zhao <chenchao.zhao@clarifai.com>
2021-10-19 14:59:52 +0800

    add logical

---
+++ argval/constraints/logicals.py
@@ -0,0 +1,36 @@
+from typing import *
+
+from .constraints import Constraint
+
+
+class NOT(Constraint):
+    def __init__(self, constraint) -> None:
+        assert isinstance(constraint, Constraint)
+        self.constraint = constraint
+
+    def assertion(self, x: Any) -> bool:
+        return ~self.constraint(x)
+
+
+class ANY(Constraint):
+    def __init__(self, *constraints) -> None:
+        for c in constraints:
+            assert isinstance(c, Constraint)
+
+        self.constraints = constraints
+
+    def assertion(self, x: Any) -> bool:
+
+        return any(c(x) for c in self.constraints)
+
+
+class ALL(Constraint):
+    def __init__(self, *constraints) -> None:
+        for c in constraints:
+            assert isinstance(c, Constraint)
+
+        self.constraints = constraints
+
+    def assertion(self, x: Any) -> bool:
+
+        return all(c(x) for c in self.constraints)
