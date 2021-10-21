b7ee2cf1e189e2729eecc81929dee460a2acd30f
Chenchao Zhao <chenchao.zhao@clarifai.com>
2021-10-21 16:30:04 +0800

    add default

--- argval/constraints/inputs.py
+++ argval/constraints/inputs.py
@@ -10,3 +10,31 @@ class Required(Constraint):
             return False
 
         return True
+
+
+class Default(Constraint):
+    """Default: Pass default value to `Argument` class.
+    No constraint imposed.
+
+    Parameters
+    ----------
+    default_value : Any
+        The default value for the argument
+
+    Attributes
+    ----------
+    value : Any
+        The default value
+
+    """
+
+    def __init__(self, default_value: Any):
+        self.value = default_value
+
+    def assertion(self, x: Any):
+
+        # no constraints
+        return True
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}(default_value={self.value})"
